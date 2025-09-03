c124bc34d7f94f402642c8e6233f919fb91e03cc
Filipe <filipeschausst@gmail.com>
2025-09-03 12:53:59 -0300

    Começo da criação do menu e da função "Botão"

--- forca.c
+++ forca.c
@@ -4,15 +4,19 @@
 #define SW 1366
 #define SH 768
 
+// Telas do jogo
 typedef enum telas {
     MENU, 
-    JOGO,
-    TECLADO
+    FORCA,
+    TECLADO,
+    FIM_JOGO
 }telas;
 
-telas atual = JOGO;
+telas tela_atual = MENU;
+bool mostrar_grades = false;
 
 int menu();
+bool botao(Rectangle retangulo_maior, float porsentagem_retangulo_menor, char *texto, Color externo, Color interno, Color texto_cor);
 
 int main(){
 
@@ -20,25 +24,35 @@ int main(){
 
     SetTargetFPS(60);
 
+    // Carrega imagem de fundo
     Texture2D quadro = LoadTexture("imagens/quadro.jpg");
 
+    // Loop principal
     while(!WindowShouldClose()){
 
         BeginDrawing();
         ClearBackground(RAYWHITE);
 
+        // Desenha tela de fundo
         DrawTextureEx(quadro, (Vector2){0,0}, 0.0, (float)SW / quadro.width, WHITE);
 
-        char buffer[100];
-        sprintf(buffer, "%d", (int)atual);
-        DrawText(buffer, SW/ 2, SH / 2, 50, BLACK);
+        // Para Testes
+        if(mostrar_grades){
+            for(int i = 0; i < SW; i += SW * 0.1){
+                DrawRectangle(i, 0, SW * 0.005, SH, BLUE);
+            }
 
-        for(int i = SW * 0.02; i < SW; i += SW * 0.05){
-            DrawRectangle(i, 0, SW * 0.005, SH, BLUE);
+            for(int i = 0; i < SH; i += SH * 0.1){
+                DrawRectangle(0, i, SW, SH * 0.005, RED);
+            }
         }
+        
+        // Selecão de telas
+        switch(tela_atual){
 
-        for(int i = 0; i < SH; i += SH * 0.05){
-            DrawRectangle(0, i, SW, SH * 0.005, RED);
+            default:
+                menu();
+                break;
         }
 
         EndDrawing();
@@ -46,4 +60,29 @@ int main(){
 
     UnloadTexture(quadro);
     CloseWindow();
+}
+
+int menu(){
+
+    //DrawRectangle(SW / 2, SH / 2, 200, 100, (Color){255,255,255, 50});
+    //DrawRectangle(SW / 2 + 25, SH / 2 + 25, 150, 50, (Color){255,255,255, 50});
+
+    botao((Rectangle){SW / 2, SH / 2, 200, 100}, 0.1, "", BLACK, BLUE, WHITE);
+
+    return 0;
+}
+
+bool botao(Rectangle retangulo_maior, float margin, char *texto, Color externo, Color interno, Color texto_cor){
+
+    DrawRectangleRec(retangulo_maior, externo);
+
+    float diferenca = retangulo_maior.height * margin;
+
+    retangulo_maior.x += diferenca / 2;
+    retangulo_maior.y += diferenca / 2;
+    retangulo_maior.height -= diferenca;
+    retangulo_maior.width -= diferenca;
+
+    DrawRectangleRec(retangulo_maior, interno);
+
 }
